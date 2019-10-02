9def013c518463fef994aa60e42842146c83ce21
lemoncmd <lemoncmd@gmail.com>
2019-10-02 23:59:10 +0900

    add the other files on the splitting

---
+++ src/parser.v
@@ -0,0 +1,109 @@
+module main
+
+struct Parser {
+  tokens []Tok
+mut:
+  pos int
+}
+
+enum Nodekind {
+  add
+  sub
+  mul
+  div
+  num
+}
+
+struct Node {
+  kind Nodekind
+  left &Node
+  right &Node
+  num int
+}
+
+fn (p mut Parser) consume(op string) bool {
+  token := p.tokens[p.pos]
+  if token.kind != .reserved || token.str != op {
+    return false
+  }
+  p.pos++
+  return true
+}
+
+fn (p mut Parser) expect(op string) {
+  token := p.tokens[p.pos]
+  if token.kind != .reserved || token.str != op {
+    unexp_err(token, 'Expected $op but got ${token.str}')
+  }
+  p.pos++
+  return
+}
+
+fn (p mut Parser) expect_number() int {
+  token := p.tokens[p.pos]
+  if token.kind != .num {
+    unexp_err(token, 'Expected number')
+  }
+  p.pos++
+  return token.str.int()
+}
+
+fn (p Parser) new_node(kind Nodekind, left, right &Node) &Node {
+  node := &Node{
+    kind:kind
+    left:left
+    right:right
+    num:0
+  }
+  return node
+}
+
+fn (p Parser) new_node_num(num int) &Node {
+  node := &Node{
+    kind:Nodekind.num
+    left:0
+    right:0
+    num:num
+  }
+  return node
+}
+
+fn (p mut Parser) expr() &Node {
+  mut node := p.mul()
+
+  for {
+    if p.consume('+') {
+      node = p.new_node(.add, node, p.mul())
+    } else if p.consume('-') {
+      node = p.new_node(.sub, node, p.mul())
+    } else {
+      return node
+    }
+  }
+  return node
+}
+
+fn (p mut Parser) mul() &Node {
+  mut node := p.primary()
+
+  for {
+    if p.consume('*') {
+      node = p.new_node(.mul, node, p.primary())
+    } else if p.consume('/') {
+      node = p.new_node(.div, node, p.primary())
+    } else {
+      return node
+    }
+  }
+  return node
+}
+
+fn (p mut Parser) primary() &Node {
+  if p.consume('(') {
+    node := p.expr()
+    p.expect(')')
+    return node
+  }
+  return p.new_node_num(p.expect_number())
+}
+
---
+++ src/token.v
@@ -0,0 +1,62 @@
+module main
+
+struct Tok {
+  kind Token
+  str string
+  line int
+  pos int
+}
+
+enum Token {
+  eof
+  reserved
+  num
+}
+
+fn new_token(token Token, s string, line, pos int) Tok {
+  return Tok{token, s, line, pos}
+}
+
+fn tokenize(p string) []Tok {
+  mut tokens := []Tok
+  mut pos := 0
+  mut line := 1
+  mut lpos := 0
+
+  for pos < p.len {
+    if p[pos] == `\n` {
+      pos++
+      line++
+      lpos = 0
+      continue
+    }
+
+    if p[pos].is_space() {
+      pos++
+      lpos++
+      continue
+    }
+
+    if p[pos] == `+` || p[pos] == `-` || p[pos] == `*` || p[pos] == `/` || p[pos] == `(` || p[pos] == `)` {
+      tokens << new_token(.reserved, p[pos++].str(), line, lpos)
+      lpos++
+      continue
+    }
+
+    if p[pos].is_digit() {
+      start_pos := pos
+      for pos < p.len && p[pos].is_digit() {
+        pos++
+        lpos++
+      }
+      tokens << new_token(.num, p.substr(start_pos, pos), line, lpos)
+      continue
+    }
+
+    parse_err('$line:$lpos: Cannot tokenize')
+  }
+
+  tokens << new_token(.eof, '', line, lpos)
+  return tokens
+}
+
